d0cb14d13f662cd22dc05b23564a64270c15ec77
Kayla Blosser <kayla.blosser@proton.me>
2026-04-24 21:06:00 -0400

    move files to better locations

--- scripts/signalBus.gd
+++ scripts/signalBus.gd
@@ -3,3 +3,5 @@ extends Node
 signal returnToReality();
 signal loadLevel(levelIdentifier: Variant);
 signal gameStart();
+signal newDialouge(character: String, dialouge: String);
+signal updateDialouge(dialouge: String);
