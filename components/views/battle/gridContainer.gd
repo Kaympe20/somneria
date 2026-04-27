6b5d1eef24be0abc49cc5ced7eb28c60f28a0c58
Kayla Blosser <kayla.blosser@proton.me>
2026-04-26 22:26:23 -0400

    moving in the positive direction now works

--- components/views/battle/gridContainer.gd
+++ components/views/battle/gridContainer.gd
@@ -1,6 +1,6 @@
 extends GridContainer
 
-var currentUnderlinePosition: Vector2 = Vector2.ONE;
+var currentUnderlinePosition: Vector2 = Vector2.ZERO;
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,29 +10,57 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var amountToMoveBy: Vector2 = Vector2.ZERO;
 	
 	if Input.is_action_pressed("right"):
-		pass
+		amountToMoveBy.x += 1;
 	if Input.is_action_pressed("left"):
-		pass
+		amountToMoveBy.x -= 1;
 	if Input.is_action_pressed("forward"):
-		pass
+		amountToMoveBy.y += 1;
 	if Input.is_action_pressed("back"):
-		pass
+		amountToMoveBy.y -= 1;
+		
+	if amountToMoveBy != Vector2.ZERO:
+		moveUnderline(amountToMoveBy);
 
 func renderOptions(options: Array[String]):
 	assert(options.size() <= 4, "renderOptions recieved an array with more than 4 values.");
 	for index in range(4):
+		
 		var text: String;
 		var label: RichTextLabel = get_node("Label%s" % str(index + 1));
 		
 		if (index < options.size()):
 			text = options[index];
+			if index == 0:
+				text = addUnderline(text);
 		else:
-			text = "            ";
+			text = "";
 			
 		label.text = text;
 
 #TODO Implement
-func moveUnderline(amountToMoveBy: Vector2) -> bool: ## Moves the underline by the amount given as a Vector 2, does nothing if the move would be out of bounds
-	return true
+func moveUnderline(amountToMoveBy: Vector2): ## Moves the underline by the amount given as a Vector 2, does nothing if the move would be out of bounds
+	var workingIndex = convertToIndex(currentUnderlinePosition);
+	
+	var label: RichTextLabel = get_node("Label%s" % (workingIndex + 1));
+	label.text = label.get_parsed_text(); # Clear underline
+	
+	print("current index is %s and index to move by is %s" % [workingIndex, convertToIndex(amountToMoveBy)]);
+	workingIndex += convertToIndex(amountToMoveBy);
+	
+	if workingIndex > 3:
+		workingIndex = 3;
+	
+	print("Getting node Label%s" % (workingIndex + 1));
+	label = get_node("Label%s" % (workingIndex + 1)); # Reassign "label" to new active RichTextLabel
+	
+	label.text = addUnderline(label.text);
+	
+func addUnderline(textToUnderline: String) -> String:
+	return "[u]%s[/u]" % textToUnderline;
+	
+func convertToIndex(elementPosition: Vector2) -> int: ## Converts from a 2D vector to an integer index indicating which label a Vector2 refers to
+	return elementPosition.x + (elementPosition.y * -2);
+	
