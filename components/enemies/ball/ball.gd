70f64e0b36bd56d23ac1b263a3f427c16ce7d077
Kayla Blosser <kayla.blosser@proton.me>
2026-05-07 23:39:58 -0400

    allow children classes of enemy to run things after parent ready

--- components/enemies/ball/ball.gd
+++ components/enemies/ball/ball.gd
@@ -3,6 +3,12 @@ extends enemy
 func _init() -> void:
 	self.headshot = load("res://assets/characters/miku/miku.png");
 	self.name = "ball";
-	self.attacks = [attack.new("drop", "sphereAction", 30.0)];
+	self.attacks = [attack.new("drop", "SphereAction", 30.0)];
 	self.model = load("res://components/enemies/ball/worst ball bounce ever.glb");
 	self.health = 100;
+
+func _postReady():
+	for child in modelInstance.get_children():
+		if child is MeshInstance3D:
+			child.position.y = 1.0;
+			
